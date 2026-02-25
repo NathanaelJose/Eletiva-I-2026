94b6f37a1f5e7c7cae10e077ea62b3ec26a3215b
Nathanael José De Souza <nathanaeljdsouza2@gmail.com>
2026-02-24 22:01:53 -0300

    exercício 4 feito

--- 24-02/exercício4.php
+++ 24-02/exercício4.php
@@ -26,12 +26,17 @@
             if($_SERVER['REQUEST_METHOD'] == "POST"){
                 $valor1 = $_POST['valor1'];
                 $valor2 = $_POST['valor2'];
+                if($valor2 == 0){
+                echo "O resultado zero!";
+                }
+                else{
                 $divisao = $valor1 / $valor2;
                 echo "A divisão é: $divisao";
+                };
             }
         ?>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
     </div>
 </body>
 
-</html>
+</html
