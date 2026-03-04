56ab1206af903f8613954f20d4215b76150648b0
Nathanael José De Souza <nathanaeljdosouza2@gmail.com>
2026-03-03 21:42:38 -0300

    acrésimo do for

--- Aula-03-03-26/exemplo.php
+++ Aula-03-03-26/exemplo.php
@@ -50,6 +50,11 @@
                 echo "<p>Sinal inválido!</p>";
                 break;
         }
+
+        for($i=1; $i<=10; $i++){
+            $resultado = $valor * $i;
+            echo "<p> $valor * $i = $resultado </p>";
+        }
     }
 ?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
