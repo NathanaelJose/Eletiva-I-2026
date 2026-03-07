52107bc30e2bdcfd45386ed219d04c6bea265f3d
Nathanael José De Souza <nathanaeljdsouza2@gmail.com>
2026-03-07 11:57:50 -0300

    exercício11feito

---
+++ 24-02/exercício11.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Exercício 11</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body> 
+<div class="container py-3">
+<h1>Exercício 11</h1>
+<form method="post">
+<div class="mb-3">
+              <label for="valor1" class="form-label">Digite o raio do retângulo:</label>
+              <input type="number" id="valor1" name="valor1" class="form-control" required="">
+            </div>
+<button type="submit" class="btn btn-primary">Enviar</button>
+</form>
+<?php
+    if ($_SERVER['REQUEST_METHOD'] == "POST")
+    {
+        $valor1 = $_POST['valor1'];
+        $pi = 3.14159;
+        $calc = 2 * $pi * $valor1;
+
+        echo "O raio do círculo é: ". $calc;
+    }
+?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</div>
+</body>
+</html>
