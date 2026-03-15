7a015b962e1e4eb753eceaf06e262341db71b0a1
Nathanael José De Souza <nathanaeljdsouza2@gmail.com>
2026-03-15 11:39:51 -0300

    exercício 2 reajustado

--- Aula-03-03-26/exercício2.php
+++ Aula-03-03-26/exercício2.php
@@ -22,7 +22,6 @@
 <?php
 if($_SERVER['REQUEST_METHOD'] == "POST")
 {
-{
     $primeiro_valor = $_POST['primeiro_valor'];
     $segundo_valor = $_POST['segundo_valor'];
 
@@ -38,7 +37,6 @@ if($_SERVER['REQUEST_METHOD'] == "POST")
     {
         echo "<p>Soma dos valores fica em: $soma</p>";
     }
-}
 }
 ?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
