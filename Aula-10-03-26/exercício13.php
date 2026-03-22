7c462d0db5ab2aa04f13c8967971f7c4f1a5577e
Nathanael José De Souza <nathanaeljdsouza2@gmail.com>
2026-03-22 16:00:46 -0300

    exercício 13

---
+++ Aula-10-03-26/exercício13.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Exercício 13</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body> 
+<div class="container py-3">
+<h1>Exercício 13</h1>
+<form method="post">
+<div class="mb-3">
+              <label for="frase" class="form-label">Digite uma frase:</label>
+              <input type="text" id="frase" name="frase" class="form-control" required="">
+            </div>
+<button type="submit" class="btn btn-primary">Enviar</button>
+</form>
+<?php
+if($_SERVER['REQUEST_METHOD'] == "POST")
+{
+    $frase = $_POST['frase'];
+
+    $palavras = explode(" ", trim($frase));
+    $total = 0;
+    $maior = "";
+
+    for($i = 0; $i < count($palavras); $i++)
+    {
+        if($palavras[$i] != "")
+        {
+            $total++;
+
+            if(strlen($palavras[$i]) > strlen($maior))
+            {
+                $maior = $palavras[$i];
+            }
+        }
+    }
+
+    echo "<p>Frase: $frase</p>";
+    echo "<p>Total de palavras: $total</p>";
+    echo "<p>Maior palavra: $maior</p>";
+}
+?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</div>
+</body>
+</html>
