58efce909897bde71c26cddfa686c5a06c57df50
Nathanael José De Souza <nathanaeljdsouza2@gmail.com>
2026-03-22 16:53:09 -0300

    exercício 04 da lista 5 feito

---
+++ Aula-17-03-26/exercício5.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Exercício 05</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body> 
+<div class="container py-3">
+<h1>Exercício 05</h1>
+<form method="post">
+            <?php
+            for($i=0; $i<5; $i++)
+            echo '<div class="mb-3">
+                <label>Nome do item:</label>
+                <input type="text" name="nome[]" class="form-control" required="">
+
+                <label>Preço:</label>
+                <input type="number" step="0.01" name="preco[]" class="form-control" required="">
+            </div>';
+?>
+<button type="submit" class="btn btn-primary">Enviar</button>
+</form>
+<?php
+if($_SERVER['REQUEST_METHOD'] == "POST")
+{
+    $nomes = $_POST['nome'];
+    $precos = $_POST['preco'];
+
+    $itens = [];
+
+    for($i = 0; $i < count($nomes); $i++)
+    {
+        $precoComImposto = $precos[$i] * 1.15; // +15%
+
+        $itens[$nomes[$i]] = $precoComImposto;
+    }
+
+    asort($itens); // ordena pelos preços (menor → maior)
+
+    echo "<p><strong>Lista de itens com imposto:</strong></p>";
+
+    foreach($itens as $nome => $preco)
+    {
+        echo "<p>$nome : R$ $preco</p>";
+    }
+}
+?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</div>
+</body>
+</html>
